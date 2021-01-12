1d94030dec7fe2050354d4705b4465dff683bfca
zhaomanzhou <zmzsstreet@163.com>
2021-01-12 18:59:17 +0800

    mock数据改为直接调用

--- src/pages/dashboard/service.ts
+++ src/pages/dashboard/service.ts
@@ -1,5 +1,6 @@
 import request from 'umi-request';
+import _mock from '@/pages/dashboard/_mock';
 
 export async function fakeChartData() {
-    return request('/api/fake_chart_data');
+    return _mock['GET  /api/fake_chart_data'];
 }

c19185e9f9dd01eae24028c5db12e70b2fe1965d
zhaomanzhou <zmzsstreet@163.com>
2021-03-24 11:51:49 +0800

    添加流量使用查看

--- src/pages/online/Oneline.tsx
+++ src/pages/online/Oneline.tsx
@@ -1,56 +1,79 @@
 import React from 'react';
-import { Alert, Button, Space, Tag, Tooltip } from 'antd';
-import { CloudServerOutlined, DownOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { Alert, Button, Card, Space, Statistic, Tag, Tooltip } from 'antd';
+import {
+    ArrowUpOutlined,
+    CloudServerOutlined,
+    DownOutlined,
+    QuestionCircleOutlined,
+} from '@ant-design/icons';
 import type { ProColumns } from '@ant-design/pro-table';
 import ProTable, { TableDropdown } from '@ant-design/pro-table';
 import request from 'umi-request';
-import { timestampToDateStr } from '@/utils/utils';
-import { Fragment } from 'react';
 
-/*
-{
-account: {
-accountNo: "1eiewr8",
-bandwidth: 100,
-createTime: 1616248667747,
-cycle: 30,
-fromDate: 1616245200000,
-id: 1169,
-level: 0,
-maxConnection: 256,
-speed: 10240,
-status: 1,
-subscriptionUrl: "/subscribe/iujc1pm5d6?type=0&timestamp=1616249026297&token=5975e1778eeed4428dd34756629b0fff",
-toDate: 1618923600000,
-updateTime: 1616252468426,
-userId: 1168,
-uuid: "6c623174-c38b-40b4-bd70-af766516bf6e"
-},
-connectionStat: {
-cache: {
-bwggia.idofast.com: {
-count: 1,
-lastSetTime: 1616504149425
-}
-},
-lastBlock: 0,
-total: 1
-},
-user: {
-createTime: 1616248667740,
-email: "2075128522@qq.com",
-id: 1168,
-password: "88d1ef86f1c9a3b33cdfc8bd5e5ef64e",
-remark: "微信shiny-月付",
-role: "vip",
-status: 1,
-updateTime: 1616322066757
-}
-}
- */
+const bgColors = ['#ffe6e6', '#ffede6', '#e6f0ff', '#e9e6ff', '#f5e6ff', '#fbe6ff', '#ffe6e6'];
+const borderColors = ['#ff9191', '#ffb491', '#91bdff', '#9193ff', '#bb91ff', '#d891ff', '#ff9191'];
 
-const colors = ['success', 'info', 'error', 'warning'];
-const servers = [];
+const serverNameMap = {
+    bwggia: '美国线路1',
+    tx: '香港线路2-腾讯云',
+    hk1: '香港线路3-阿里云',
+    hk2: '香港线路4-六六云',
+    hk5: '香港线路5-UOZ',
+    hk7: '香港线路7-Gigs',
+    hkvip: '香港线路6-cloudiplc',
+    us1: '美国线路2-rk',
+    jpgia: '日本线路1-搬瓦工',
+    sa1: '南非线路1',
+    m1: '中转线路1',
+    iplc1: '中转线路2-IPLC',
+    korean1: '韩国线路1-狗云',
+    korean2: '韩国线路2-月抛',
+    idofast: '香港线路1-阿里云',
+};
+
+const ServerComponment = (entity, style) => {
+    let cahche = Object.keys(entity.connectionStat.cache);
+
+    return (
+        <div>
+            <Space direction="vertical">
+                {cahche.map((e, index) => {
+                    let sum = 0;
+                    for (let i = 0; i < 4; i++) {
+                        sum += e.charCodeAt(i);
+                    }
+
+                    let prefix = e.split('.')[0];
+                    return (
+                        <Alert
+                            key={e}
+                            message={
+                                style == 1
+                                    ? serverNameMap[prefix]
+                                        ? serverNameMap[prefix]
+                                        : prefix
+                                    : entity.connectionStat.cache[e].count
+                            }
+                            showIcon
+                            icon={
+                                <CloudServerOutlined
+                                    style={{
+                                        fontSize: 15,
+                                        color: borderColors[sum % bgColors.length],
+                                    }}
+                                />
+                            }
+                            style={{
+                                backgroundColor: bgColors[sum % bgColors.length],
+                                borderColor: borderColors[sum % bgColors.length],
+                            }}
+                        />
+                    );
+                })}
+            </Space>
+        </div>
+    );
+};
 
 const columns: ProColumns[] = [
     {
@@ -87,57 +110,22 @@ const columns: ProColumns[] = [
         key: 'option',
         valueType: 'option',
         render: (_, entity) => {
-            let cahche = Object.keys(entity.connectionStat.cache);
-
-            return (
-                <div>
-                    <Space direction="vertical">
-                        {cahche.map((e, index) => {
-                            let sum = 0;
-                            for (let i = 0; i < 4; i++) {
-                                sum += e.charCodeAt(i);
-                            }
-                            return (
-                                <Alert
-                                    key={e}
-                                    message={e.split('.')[0]}
-                                    type={colors[sum % colors.length]}
-                                    showIcon
-                                    icon={<CloudServerOutlined style={{ fontSize: 15 }} />}
-                                    style={{}}
-                                ></Alert>
-                            );
-                        })}
-                    </Space>
-                </div>
-            );
+            return ServerComponment(entity, 1);
+        },
+        sorter: (a, b) => {
+            let keys1 = Object.keys(a.connectionStat.cache);
+            let keys2 = Object.keys(b.connectionStat.cache);
+            if (keys1.length == keys2.length && keys2.length == 0) {
+                return 0;
+            } else {
+                return keys1[0] > keys2[0] ? -1 : 1;
+            }
         },
     },
     {
         title: '连接数',
         render: (_, entity) => {
-            let cahche = Object.keys(entity.connectionStat.cache);
-            return (
-                <div>
-                    <Space direction="vertical">
-                        {cahche.map((e, index) => {
-                            let sum = 0;
-                            for (let i = 0; i < 4; i++) {
-                                sum += e.charCodeAt(i);
-                            }
-                            return (
-                                <Alert
-                                    key={e}
-                                    message={entity.connectionStat.cache[e].count}
-                                    type={colors[sum % colors.length]}
-                                ></Alert>
-                            );
-                        })}
-                    </Space>
-                </div>
-            );
-
-            return entity.user.remark;
+            return ServerComponment(entity, 2);
         },
     },
 
@@ -203,6 +191,9 @@ export default () => {
             search={false}
             dateFormatter="string"
             headerTitle="表格标题"
+            pagination={{
+                pageSize: 50,
+            }}
         />
     );
 };

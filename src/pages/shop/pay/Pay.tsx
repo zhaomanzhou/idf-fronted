1b04ece66e8f499c546b65353facf8c666de7da7
zhaomanzhou <zmzsstreet@163.com>
2021-04-18 23:09:41 +0800

    发布dist

--- src/pages/shop/pay/Pay.tsx
+++ src/pages/shop/pay/Pay.tsx
@@ -69,12 +69,15 @@ export default () => {
         <PageContainer title={'订单结算'}>
             <div className={styles.card}>
                 <p className={styles.price}> {'应付金额 ' + payInfo.totalMoney * 0.01 + '元'}</p>
-                <QRCode
-                    value={payInfo.payLink} // 需要生成二维码图片的url地址
-                    size={300} // 二维码图片大小
-                    fgColor="#000000" // 二维码图片背景色
-                    className={styles.qr}
-                />
+                <a href={payInfo.payLink}>
+                    <QRCode
+                        value={payInfo.payLink} // 需要生成二维码图片的url地址
+                        size={300} // 二维码图片大小
+                        fgColor="#000000" // 二维码图片背景色
+                        className={styles.qr}
+                    />
+                </a>
+
                 <p className={styles.info}> {'打开支付宝 扫描二维码进行支付'}</p>
             </div>
         </PageContainer>

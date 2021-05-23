29c9418d0afe00e19ab4cfe1c8fee7579413ea9d
zhaomanzhou <zmzsstreet@163.com>
2021-05-23 22:50:28 +0800

    发版

--- config/routes.ts
+++ config/routes.ts
@@ -132,7 +132,7 @@
                                 path: '/server/v2ray/admin',
                                 name: 'V2ray',
                                 icon: 'ToolOutlined',
-                                component: './server/v2ray/admin/V2rayNode',
+                                component: './server/v2ray/admin/V2rayNode_Protable',
                             },
                         ],
                         authority: ['admin'],
